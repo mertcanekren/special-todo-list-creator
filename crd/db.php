947f64ca0da60de6e2ec546166897a5f4b77f5b3
Mertcan Ekren <mertcanekren@windowslive.com>
2013-03-04 09:48:20 +0200

    error reporting 0

--- crd/db.php
+++ crd/db.php
@@ -3,8 +3,10 @@
 	 * @author      Mertcan Ekren
 	 * @license		http://en.wikipedia.org/wiki/MIT_License
 	 */
+	error_reporting(0);
+	ini_set('display_errors', false);
 	$db = new SQLite3('tododatabase.db',SQLITE3_OPEN_READWRITE);
-	if($_POST){
+
 		if($_POST["todoid"] && $_POST["process"]){
 			if($_POST["process"] == "passive"){
 				if($db->exec("update todo set status='0' where id=".$_POST["todoid"])){
@@ -13,15 +15,13 @@
 					echo json_encode(array('status' => false));
 				}
 			}
-		}
-		if($_POST["listid"] && $_POST["todo"]){
+		}elseif($_POST["listid"] && $_POST["todo"]){
 			if($db->exec("insert into todo (title, createtime, projectid, status) values ('".$_POST["todo"]."', '".time()."', '".$_POST["listid"]."', '".true."')" )){
 				echo json_encode(array('status' => '1','todo' => $_POST["todo"], 'todoid' => $db->lastInsertRowID()));
 			}else{
 				echo json_encode(array('status' => false));
 			}
-		}
-		if($_POST["listname"]){
+		}elseif($_POST["listname"]){
 			$id = uniqid();
 			if($db->exec("insert into projects (name) values ( '".$_POST["listname"]."')")){
 				echo json_encode(array('status' => true,'listname' => $_POST["listname"], 'listid' => $db->lastInsertRowID()));
@@ -29,5 +29,5 @@
 				echo json_encode(array('status' => false));
 			}
 		}
-	}
+
 ?>
