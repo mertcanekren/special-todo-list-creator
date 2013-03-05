baea2589d467325ab6a026bf357aa06c25217f8b
Mertcan Ekren <mertcanekren@windowslive.com>
2013-03-05 14:16:37 +0200

    delete create table query

--- crd/resetdb.php
+++ crd/resetdb.php
@@ -8,9 +8,4 @@
 	$db->exec("delete from projects");
 	$db->exec("delete from todo");
 
-
-	$db->exec('create table projects (id integer PRIMARY KEY, name STRING)');
-	$db->exec('create table todo (id integer PRIMARY KEY, title STRING, createtime STRING, projectid STRING, status STRING)');
-	
-
 ?>
